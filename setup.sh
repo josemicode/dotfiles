3fa05a2c136bad38d4fc08f00c7e8990d06345bf
Jose <95041130+josemicode@users.noreply.github.com>
2024-10-28 11:28:45 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,6 +5,6 @@ ln -sf ~/dotfiles/hyprland.conf ~/.config/hypr/hyprland.conf
 ln -sf ~/dotfiles/hyprpaper.conf ~/.config/hypr/hyprpaper.conf
 ln -sf ~/dotfiles/neofetch ~/.config/neofetch
 
-# Add other symlinks as needed
+# Add symlinks here as new config files are made
 
 echo "Dotfiles setup complete!"
