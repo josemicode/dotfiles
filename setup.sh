c75a986c909cf46864db879d365699320a260ba7
josemicode <jmiguelojedagutierrez@gmail.com>
2026-01-17 20:47:22 +0100

    Added Zed Config

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ ln -sf ~/dotfiles/hyprpaper.conf ~/.config/hypr/hyprpaper.conf
 ln -sf ~/dotfiles/neofetch ~/.config/neofetch
 ln -sf ~/dotfiles/wallpapers/wall_selector.sh ~/wallpapers/wall_selector.sh
 ln -sf ~/dotfiles/vscodium/User ~/.config/VSCodium/User
+ln -sf ~/dotfiles/zed ~/.config/zed
 
 # Add other symlinks as needed
 
